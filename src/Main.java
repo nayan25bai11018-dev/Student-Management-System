57142d8856a89584b9aa507411337e3c7273f3d7
Nayan Kumar Gupta <nayan.25bai11018@vitbhopal.ac.in>
2026-09-17 02:53:22 +0530

    Complete student management module

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,105 @@
 import model.Student;
 import service.StudentService;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        StudentService studentService= new StudentService();
-        Student student1 = new Student(
-                101,
-                "Rahul",
-                19,
-                "CSE",
-                2
-        );
-        studentService.addStudent(student1);
+        Scanner scanner = new Scanner(System.in);
+        StudentService studentService = new StudentService();
+        while (true) {
 
-        System.out.println("Students:");
-        studentService.viewStudents();
+            System.out.println("\n==============================");
+            System.out.println("   STUDENT MANAGEMENT SYSTEM");
+            System.out.println("================================");
+            System.out.println("1. Add Student");
+            System.out.println("2. View Students");
+            System.out.println("3. Search Student");
+            System.out.println("4. Update Student");
+            System.out.println("5. Delete Student");
+            System.out.println("6. Exit");
+            System.out.print("Enter your choice: ");
+            int choice = scanner.nextInt();
+            switch (choice) {
+
+                case 1:
+                    System.out.print("Enter student ID: ");
+                    int id = scanner.nextInt();
+                    scanner.nextLine();
+
+                    System.out.print("Enter student name: ");
+                    String name = scanner.nextLine();
+
+                    System.out.print("Enter age: ");
+                    int age = scanner.nextInt();
+                    scanner.nextLine();
+
+                    System.out.print("Enter branch: ");
+                    String branch = scanner.nextLine();
+
+                    System.out.print("Enter semester: ");
+                    int semester = scanner.nextInt();
+
+                    Student student = new Student(
+                            id, name, age, branch, semester
+                    );
+
+                    studentService.addStudent(student);
+
+                    System.out.println("Student added successfully!");
+                    break;
+                case 2:
+                    System.out.println("\n---- STUDENT LIST ----");
+                    studentService.viewStudents();
+                    break;
+                case 3:
+                    System.out.print("Enter student ID to search: ");
+                    int searchId = scanner.nextInt();
+
+                    studentService.searchStudent(searchId);
+                    break;
+                case 4:
+                    System.out.print("Enter student ID to update: ");
+                    int updateId = scanner.nextInt();
+
+                    scanner.nextLine();
+
+                    System.out.print("Enter new name: ");
+                    String newName = scanner.nextLine();
+
+                    System.out.print("Enter new age: ");
+                    int newAge = scanner.nextInt();
+
+                    scanner.nextLine();
+
+                    System.out.print("Enter new branch: ");
+                    String newBranch = scanner.nextLine();
+
+                    System.out.print("Enter new semester: ");
+                    int newSemester = scanner.nextInt();
+
+                    studentService.updateStudent(
+                            updateId,
+                            newName,
+                            newAge,
+                            newBranch,
+                            newSemester
+                    );
+
+                    break;
+                case 5:
+                    System.out.print("Enter student ID to delete: ");
+                    int deleteId = scanner.nextInt();
+
+                    studentService.deleteStudent(deleteId);
+                    break;
+                case 6:
+                    System.out.println("Thank you");
+                    scanner.close();
+                    return;
+
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        }
     }
 }
--- src/service/StudentService.java
+++ src/service/StudentService.java
@@ -14,4 +14,44 @@ public class StudentService{
             System.out.println(student);
         }
     }
+    public void searchStudent(int id) {
+        for (Student student : students) {
+            if (student.getId() == id) {
+                System.out.println("Student found:");
+                System.out.println(student);
+                return;
+            }
+        }
+
+        System.out.println("Student not found");
+    }
+    public void updateStudent(int id, String name, int age, String branch, int semester) {
+
+        for (Student student : students) {
+
+            if (student.getId() == id) {
+                student.setName(name);
+                student.setAge(age);
+                student.setBranch(branch);
+                student.setSemester(semester);
+                System.out.println("Student updated successfully.");
+                return;
+            }
+        }
+
+        System.out.println("Student not found.");
+    }
+    public void deleteStudent(int id) {
+
+        for (int i = 0; i < students.size(); i++) {
+
+            if (students.get(i).getId() == id) {
+                students.remove(i);
+                System.out.println("Student deleted successfully.");
+                return;
+            }
+        }
+
+        System.out.println("Student not found.");
+    }
 }
